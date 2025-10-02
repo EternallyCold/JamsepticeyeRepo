df1bad73c613c01742de32340243ce6e09872fc6
fortskewer <fortskewer@gmail.com>
2025-10-02 19:12:05 +0100

    fixed movement constant jumping though

--- JamsepticeyeGame/Assets/Scripts/PlayerMovement.cs
+++ JamsepticeyeGame/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] public float Speed = 1f;
+    [SerializeField] public float JumpForce = 1f;
 
-    // Update is called once per frame
-    void Update()
+    private Rigidbody2D _rb;
+
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * Speed;
+
+        if (Input.GetButtonDown("Jump") || Mathf.Abs(_rb.velocity.y) < 0.001f)
+        {
+            _rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
     }
 }
