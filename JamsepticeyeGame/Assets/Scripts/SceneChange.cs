bd1890e3fc075847c6fdd951ca7c333dc32ca0b0
EternallyCold <eternallycold03@gmail.com>
2025-10-02 16:03:44 +0100

    scenechange script added

--- JamsepticeyeGame/Assets/Scripts/SceneChange.cs
+++ JamsepticeyeGame/Assets/Scripts/SceneChange.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    
+    //changes the scene to whatever scene has been put in the unity editor for the button its on
+    public void ChangeScene(string targetSceneName)
+    {
+        SceneManager.LoadScene(targetSceneName);
+    }
 }
