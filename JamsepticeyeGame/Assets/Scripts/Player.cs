dc354644b970c2f48d646cd7b536abb232124b43
EternallyCold <eternallycold03@gmail.com>
2025-10-04 17:34:14 +0100

    forgive me im messing with scripts for the sake of animation

--- JamsepticeyeGame/Assets/Scripts/Player.cs
+++ JamsepticeyeGame/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     public float health = 100f;
     public bool isGhost = false;
     public float ghostTime = 10f; // in seconds, low number for testing
+    [SerializeField] private Animator animator; //added by mel to handle animation transitions
 
     private float timeSpendAsGhost = 0f;
 
@@ -24,6 +25,11 @@ public class Player : MonoBehaviour
         if (health <= 0f) {
             isGhost = true;
             health = 100f; // assuming ghosts can die
+            animator.SetBool("isGhost", true); //switches player sprite to ghost sprite animation
+        }
+        else
+        {
+            animator.SetBool("isGhost", false); //does nothing to the player sprite
         }
     }
 }
